885e1b4686cde74610fafbfea73f149aecac48d9
Piter31 <piteribarra@gmail.com>
2026-09-15 09:56:01 -0300

    lib/api modificado

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,6 @@
 const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/api";
+  // process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/api";
+  process.env.NEXT_PUBLIC_API_URL ?? "https://2c7gttsn-4000.brs.devtunnels.ms/api";
 
 export class ApiError extends Error {
   constructor(
